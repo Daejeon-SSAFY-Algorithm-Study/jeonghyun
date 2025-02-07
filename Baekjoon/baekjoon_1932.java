43dad0c22f32ae8d67ae9ca0108cf50cd6faeb67
JeongHyunHeo <jeonghyunheo619@gmail.com>
2025-02-07 23:45:14 +0900

    WEEK1: Baekjoon1932 - 정수 삼각형  solution added

---
+++ Baekjoon/baekjoon_1932.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.io.*;
+
+class baekjoon_1932 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int N = Integer.parseInt(br.readLine());
+    int[][] input = new int[N][N];
+
+    for (int i = 1; i <= N; i++) {
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      for (int j = 0; j < i; j++) {
+        input[i - 1][j] = Integer.parseInt(st.nextToken());
+      }
+    }
+
+    StringBuilder sb = new StringBuilder();
+
+    // ALGORITHM
+    int[][] arr = new int[N][N];
+
+    arr[0][0] = input[0][0];
+
+    int ans = 0;
+    if (N == 1) {
+      ans = input[0][0];
+    }
+    else {
+      for (int i = 1; i < N; i++) {
+        for (int j = 0; j < i + 1; j++) {
+          // case: 맨앞에 있는 경우, front j, j+1
+          if (j == 0) {
+            arr[i][j] = arr[i - 1][j] + input[i][j];
+          }
+          // case : 각 줄의 끝, j와 i의 index 같음
+          else if (j == i) {
+            arr[i][j] = arr[i - 1][j - 1] + input[i][j];
+          } else {
+            arr[i][j] = Math.max(arr[i - 1][j - 1] + input[i][j], arr[i - 1][j] + input[i][j]);
+          }
+
+          // only at last row get answer
+          if (i == N - 1) {
+            ans = Math.max(ans, arr[i][j]);
+          }
+        }
+      }
+    }
+
+    sb.append(ans).append("\n");
+
+    bw.write(sb.toString());
+    br.close();
+    bw.flush();
+    bw.close();
+  }
+}
--- Baekjoon/baekjoon_2138.java
+++ Baekjoon/baekjoon_2138.java
@@ -2,85 +2,83 @@ import java.io.*;
 import java.util.*;
 
 public class baekjoon_2138 {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		int n = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
-		
-		int[] current = new int[n];
-		int[] desired = new int[n];
+        int[] current = new int[n];
+        int[] desired = new int[n];
 
-		String line = br.readLine();
+        String line = br.readLine();
 
-		for(int i = 0 ; i < n; i++) {
-			current[i] = line.charAt(i) - '0';
-		}
-		
+        for (int i = 0; i < n; i++) {
+            current[i] = line.charAt(i) - '0';
+        }
 
-		line = br.readLine();
-		for(int i = 0; i < n ; i++) {
-			desired[i] = line.charAt(i) - '0';
-		}
+        line = br.readLine();
+        for (int i = 0; i < n; i++) {
+            desired[i] = line.charAt(i) - '0';
+        }
 
+        // Algorithm
+        int answer = solution(n, current, desired);
 
-		// Algorithm
-		int answer = solution(n, current, desired);
+        StringBuilder sb = new StringBuilder();
 
-		StringBuilder sb = new StringBuilder();
+        sb.append(answer).append("\n");
 
-		sb.append(answer).append("\n");
+        bw.write(sb.toString());
 
-		bw.write(sb.toString());
+        br.close();
+        bw.flush();
+        bw.close();
+    }
 
-		br.close();
-		bw.flush();
-		bw.close();
-	}
-
-	public static int solution(int n, int[] current, int[] desired) {
-		int[] case1 = current.clone();
+    public static int solution(int n, int[] current, int[] desired) {
+        int[] case1 = current.clone();
         int[] case2 = current.clone();
 
         int count1 = tryCase(n, case1, desired, false);
         int count2 = tryCase(n, case2, desired, true);
 
-        if(count1 == -1 && count2 == -1) return -1;
-        if(count1 == -1) return count2;
-        if(count2 == -1) return count1;
+        if (count1 == -1 && count2 == -1)
+            return -1;
+        if (count1 == -1)
+            return count2;
+        if (count2 == -1)
+            return count1;
 
         return Math.min(count1, count2);
 
-        
-	}
+    }
 
-    private static int tryCase(int n , int[] current, int[] desired, boolean pressFirst) {
+    private static int tryCase(int n, int[] current, int[] desired, boolean pressFirst) {
         int count = 0;
 
-        if(pressFirst) {
+        if (pressFirst) {
             current[0] = Math.abs(current[0] - 1);
             current[1] = Math.abs(current[1] - 1);
             count = 1;
-		}
+        }
 
-        for(int i =  1; i < n - 1; i++) {
-            if(current[i-1] != desired[i-1]) {
-                current[i-1] = Math.abs(current[i-1] -1);
+        for (int i = 1; i < n - 1; i++) {
+            if (current[i - 1] != desired[i - 1]) {
+                current[i - 1] = Math.abs(current[i - 1] - 1);
                 current[i] = Math.abs(current[i] - 1);
-                current[i+1] = Math.abs(current[i+1] - 1);
+                current[i + 1] = Math.abs(current[i + 1] - 1);
 
                 count++;
             }
         }
 
-        if(n > 1 && current[n-2] != desired[n-2]) {
-            current[n-2] = Math.abs(current[n-2] - 1);
-            current[n-1] = Math.abs(current[n-1] - 1);
+        if (n > 1 && current[n - 2] != desired[n - 2]) {
+            current[n - 2] = Math.abs(current[n - 2] - 1);
+            current[n - 1] = Math.abs(current[n - 1] - 1);
             count++;
         }
-        
+
         return Arrays.equals(current, desired) ? count : -1;
     }
 }
-
